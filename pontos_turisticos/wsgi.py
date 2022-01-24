3dc9c70235aed1201ceb0b6666ed59576cfef0cd
Dioo16 <Borgesdiogo54@gmail.com>
2022-01-24 20:30:27 -0300

    asaas

---
+++ pontos_turisticos/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for pontos_turisticos project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+import standalone
+
+
+standalone.run('pontos_turisticos.settings')
+application = Cling(get_wsgi_application())
+
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE')
+
+application = get_wsgi_application()
